81a1f6d5ea90a94f9f05514ff6d364c665c665c0
Nick Cook <nicholaslcook@yahoo.com>
2020-01-07 23:16:36 -0800

    added clear button

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('#container')
 const box = document.createElement('div');
-let numOfBoxes = 16;
+let numOfBoxes = 40;
 
 
 function chooseYourSize(n) {
@@ -10,10 +10,7 @@ function chooseYourSize(n) {
             box.classList.add('box');
             box.style.border = "1px solid";
             box.addEventListener("mouseover", function(){
-                if(mouseDown){
                     box.style.background = "black";
-                  }
-                
             });
             container.appendChild(box);
         }
@@ -22,6 +19,22 @@ function chooseYourSize(n) {
     container.style.gridTemplateRows = "repeat(" + n + ", 1fr)";
     
 }
+
+const clearBut = document.querySelector('#clearBut')
+clearBut.addEventListener("click", function(){
+    let backToWhite = document.getElementsByClassName("box");
+    for(let i = backToWhite.length - 1; i >= 0; i--) {
+      backToWhite[i].parentNode.removeChild(backToWhite[i]);
+    }
+    numOfBoxes = prompt("How many boxes per side?", "Max is 72 (number must be positive!)");
+    
+    if(numOfBoxes <= 0 || numOfBoxes > 72) {
+      alert("Must be a positive number, 72 or below.");
+      numOfBoxes = 6;
+    }
+
+    chooseYourSize(numOfBoxes);
+});
 
 var mouseDown = 0;
 document.body.onmousedown = function() { 
